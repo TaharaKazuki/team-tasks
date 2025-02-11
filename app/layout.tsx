ba5cb785d27ba2908ae4c5a8f29fd46e4203645f
Tahara Kazuki <>
2025-02-11 19:54:25 +0900

    Set up UI components and theming with shadcn/ui and next-themes

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,16 +1,7 @@
 import type { Metadata } from 'next';
-import { Geist, Geist_Mono } from 'next/font/google';
+
 import './globals.css';
-
-const geistSans = Geist({
-  variable: '--font-geist-sans',
-  subsets: ['latin'],
-});
-
-const geistMono = Geist_Mono({
-  variable: '--font-geist-mono',
-  subsets: ['latin'],
-});
+import { ThemeProviders } from '../components/theme-providers';
 
 export const metadata: Metadata = {
   title: 'Create Next App',
@@ -19,15 +10,15 @@ export const metadata: Metadata = {
 
 export default function RootLayout({
   children,
-}: Readonly<{
+}: {
   children: React.ReactNode;
-}>) {
+}) {
   return (
-    <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
-        {children}
+    <html lang="en" suppressHydrationWarning>
+      <body>
+        <ThemeProviders attribute="class" defaultTheme="system" enableSystem>
+          {children}
+        </ThemeProviders>
       </body>
     </html>
   );
