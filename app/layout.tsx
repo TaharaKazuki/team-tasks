2c56131a831084f7fa7dc03cf0b3985a8a6b38e5
Tahara Kazuki <>
2025-02-11 20:04:41 +0900

    Update HTML language attribute to Japanese

--- app/layout.tsx
+++ app/layout.tsx
@@ -14,7 +14,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <html lang="en" suppressHydrationWarning>
+    <html lang="ja" suppressHydrationWarning>
       <body>
         <ThemeProviders attribute="class" defaultTheme="system" enableSystem>
           {children}
